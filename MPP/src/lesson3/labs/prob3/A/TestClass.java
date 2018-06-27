326394df587e69ce69ecf6d7d97c8f9d3e063d17
Juan Francisco Mosquera <maxiplux@gmail.com>
2018-06-27 16:48:06 -0500

    Test class

---
+++ MPP/src/lesson3/labs/prob3/A/TestClass.java
@@ -0,0 +1,15 @@
+package lesson3.labs.prob3.A;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+	
+		Circle earth = new Circle(3.959);		
+		System.out.printf("The Earth(%s) Area if it  was a circle is %s mi \n",earth, earth.computeArea());
+		
+		
+		Cylinder cilinder = new Cylinder(3.959,100);		
+		System.out.printf("We have a Cilinder (%s) with Area : %s \n",cilinder, cilinder.computeArea());
+		
+	}
+}
