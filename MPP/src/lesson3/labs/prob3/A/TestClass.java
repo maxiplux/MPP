60ca0c1bd52cf3bc585e711908430f387f3002b8
Daniel Trinh <kyle.sontrinh@gmail.com>
2018-06-27 17:41:58 +0700

    Answers

--- MPP/src/lesson3/labs/prob3/A/TestClass.java
+++ MPP/src/lesson3/labs/prob3/A/TestClass.java
@@ -3,7 +3,8 @@ package lesson3.labs.prob3.A;
 public class TestClass {
 
 	public static void main(String[] args) {
-	
+		//Does it make 		sense to use inheritance here? -> Yes , 
+		//because we can delete the height on Cylinder or remove vertical dimension , to make a circle
 		Circle earth = new Circle(3.959);		
 		System.out.printf("The Earth(%s) Area if it  was a circle is %s mi \n",earth, earth.computeArea());
 		
