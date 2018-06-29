76bef518f908abbb0d84f1c8e4e1bcac34bcc7a5
Juan Francisco Mosquera <maxiplux@gmail.com>
2018-06-29 16:38:11 -0500

    Commit point number 1

---
+++ MPP/src/lesson5/labs/prob1/rulesets/RuleSetFactory.java
@@ -0,0 +1,30 @@
+package lesson5.labs.prob1.rulesets;
+
+import java.awt.Component;
+import java.util.HashMap;
+
+import lesson5.labs.prob1.gui.BookWindow;
+import lesson5.labs.prob1.gui.CDWindow;
+
+final public class RuleSetFactory {
+	private RuleSetFactory(){}
+	static HashMap<Class<? extends Component>, RuleSet> map = new HashMap<>();
+	static {
+		map.put(BookWindow.class, new BookRuleSet());
+		map.put(CDWindow.class, new CDRuleSet());
+	}
+	
+	
+	public static  RuleSet getRuleSet(Component component)
+	 {
+		 Class<? extends Component> cl = component.getClass();
+		 
+			if(!map.containsKey(cl)) 
+			{
+				throw new IllegalArgumentException("RuleSet not found for your entry");
+			}
+			return map.get(cl); 
+	 }
+	 
+	
+}
