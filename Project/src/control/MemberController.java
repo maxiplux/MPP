99a11a36335bc8a8b60ced43c9866e3c32fc3166
986580 <986580@CS_V45-302.cs.mum.edu>
2018-07-03 16:26:36 -0500

    hw

---
+++ Project/src/control/MemberController.java
@@ -0,0 +1,55 @@
+package control;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.dataaccess.DataAccess;
+import model.dataaccess.DataAccessFacade;
+import model.domain.Address;
+import model.domain.LibraryMember;
+import model.domain.User;
+import util.Util;
+
+public class MemberController {
+
+	@FXML
+	private TextField txtFirtsName;
+	@FXML
+	private TextField txtLastName;
+	@FXML
+	private TextField txtStreet;
+	@FXML
+	private TextField txtCity;
+	@FXML
+	private TextField txtState;
+	@FXML
+	private TextField txtZip;
+	@FXML
+	private TextField txtPhone;
+
+	@FXML
+	protected void addMemberButtonAction(ActionEvent event) {
+
+		DataAccess db = new DataAccessFacade();
+		HashMap<String, User> users = db.readUserMap();
+		int mId= users.size();
+		Address add= new Address(txtStreet.getText(), txtCity.getText(), txtState.getText(), txtZip.getText());
+		LibraryMember lm = new  LibraryMember(mId+"", txtFirtsName.getText(),txtLastName.getText(), txtCity.getText(), add);
+		
+		db.saveNewMember(lm);
+		
+
+	}
+
+}
