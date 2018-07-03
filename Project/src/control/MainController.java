9186215f136ad6184a41bb7266d52a08702ab8b8
Juan Francisco Mosquera <maxiplux@gmail.com>
2018-07-03 15:55:04 -0500

    fix

--- Project/src/control/MainController.java
+++ Project/src/control/MainController.java
@@ -4,12 +4,21 @@ package control;
 
  
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+import model.dataaccess.DataAccess;
+import model.dataaccess.DataAccessFacade;
 import model.domain.User;
+import util.Util;
 
 public class MainController extends Application {
     private User user;
@@ -30,6 +39,26 @@ public class MainController extends Application {
         
         stage.setTitle("FXML Welcome");
         stage.setScene(new Scene(root, 600, 275));
+        
+        
+        Button btnCheckoutBooks = (Button) root.lookup("#btnCheckoutBooks");
+		btnCheckoutBooks.setOnAction((event) -> {
+		 System.out.println("btnCheckoutBooks");
+		});
+		
+		Button btnBooks = (Button) root.lookup("#btnBooks");
+		btnBooks.setOnAction((event) -> {
+		 System.out.println("btnBooks");
+		});
+		
+		
+		Button btnLibraryMembers = (Button) root.lookup("#btnLibraryMembers");
+		btnLibraryMembers.setOnAction((event) -> {
+		 System.out.println("btnLibraryMembers");
+		});
+		
+		
+		
         stage.show();
     }
     
