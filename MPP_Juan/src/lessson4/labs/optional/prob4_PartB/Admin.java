15418af95963d52db36215543bb6d80b1466e8c7
Juan Francisco Mosquera <maxiplux@gmail.com>
2018-07-03 12:49:43 -0500

    firts commit

---
+++ MPP_Juan/src/lessson4/labs/optional/prob4_PartB/Admin.java
@@ -0,0 +1,11 @@
+package lessson4.labs.optional.prob4_PartB;
+
+import java.util.List;
+
+public class Admin {
+	public static double computeUpdatedBalanceSum(List<Employee> list) {
+		
+		return list.stream().mapToDouble(i -> i.computeUpdatedBalanceSum()).sum();
+
+	}
+}
---
+++ MPP_Juan/src/lessson4/labs/optional/prob4_PartB/SavingsAccount.java
@@ -0,0 +1,34 @@
+package lessson4.labs.optional.prob4_PartB;
+
+public class SavingsAccount extends Account {
+
+	private double balace;
+	private double interestRate;
+	private String acctId;
+	
+	public SavingsAccount(String acctId, double interestRate, double balace) {
+		// TODO Auto-generated constructor stub
+		this.acctId=acctId;
+		this.interestRate=interestRate;
+		this.balace=balace;
+	}
+
+	@Override
+	String getAccountId() {
+		// TODO Auto-generated method stub
+		return acctId;
+	}
+
+	@Override
+	double getBalance() {
+		// TODO Auto-generated method stub
+		return this.balace;
+	}
+
+	@Override
+	double computeUpdatedBalance() {
+		// TODO Auto-generated method stub
+		return this.balace+ (this.balace*this.interestRate);
+	}
+
+}
