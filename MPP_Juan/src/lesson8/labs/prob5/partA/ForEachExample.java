4d5dcb9918ca7ae47d88259100a8f4e330300133
Juan Francisco Mosquera <maxiplux@gmail.com>
2018-07-10 17:29:50 -0500

    my solutions to problems

---
+++ MPP_Juan/src/lesson8/labs/prob5/partA/ForEachExample.java
@@ -0,0 +1,22 @@
+package lesson8.labs.prob5.partA;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.function.Consumer;
+
+public class ForEachExample {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", "Away", "On Vacation",
+				"Everywhere you want to be");
+		Consumer<String> doSomething = (String s) -> System.out.println(s.toUpperCase());
+		list.forEach(doSomething);
+
+		// print each element of the list in upper case format
+
+	}
+
+	// implement a Consumer
+
+}
---
+++ MPP_Juan/src/lesson8/labs/prob6/part1/Apple.java
@@ -0,0 +1,20 @@
+package lesson8.labs.prob6.part1;
+
+public class Apple {
+	double Weight;
+
+	public Apple(double weight) {
+		super();
+		Weight = weight;
+	}
+
+	public double getWeight() {
+		return Weight;
+	}
+
+	public void setWeight(double weight) {
+		Weight = weight;
+	}
+
+	
+}
