947aaba114e9ccb651ad29edbf85e35c2c0f1b2d
Daniel Trinh <kyle.sontrinh@gmail.com>
2018-07-12 01:02:30 +0700

    Question 2

--- MPP/src/lesson8/labs/prob2/A/EmployeeInfo.java
+++ MPP/src/lesson8/labs/prob2/A/EmployeeInfo.java
@@ -6,25 +6,46 @@ import java.util.Collections;
 import java.util.List;
 
 public class EmployeeInfo {
+	static enum SortMethod {BYNAME, BYSALARY};
+	private SortMethod method;
 	
+	public EmployeeInfo(SortMethod method) {
+		this.method = method;
+	}
 	//Comparators are unaware of the value in method
 	public void sort(List<Employee> emps) {
-			Collections.sort(emps, new EmployeeComparator());
+		if(method == SortMethod.BYNAME) {
+			Collections.sort(emps, new EmployeeNameComparator());
+		}
+		else if(method == SortMethod.BYSALARY) {
+			Collections.sort(emps, new EmployeeSalaryComparator());
+		}
 	}
 	
 	public static void main(String[] args) {
+		List<Employee> emps = new ArrayList<>();
+		emps.add(new Employee("Joe", 100000));
+		emps.add(new Employee("Joe", 50000));
+		emps.add(new Employee("Andy", 60000));
 		Employee joe = new Employee("Joe", 100000);
 		Employee joe2 = new Employee("Joe", 200000);
-		Employee joe3 = new Employee("Joe", 100000);
+		List<Employee> list = Arrays.asList(joe, joe2);
+		Collections.sort(list, new EmployeeNameComparator());
+		System.out.println(list);
+		
 		
 		System.out.println("joe equals joe2? " + joe.equals(joe2));
-		EmployeeComparator c = new EmployeeComparator();
+		EmployeeNameComparator c = new EmployeeNameComparator();
 		System.out.println("joe equals joe2? " + (c.compare(joe, joe2)==0) );
-		
-		System.out.println("joe equals joe3? " + joe.equals(joe3));
-		EmployeeComparator c2 = new EmployeeComparator();
-		System.out.println("joe equals joe3? " + (c2.compare(joe, joe3)==0) );
 
+		
+		/*
+		EmployeeInfo ei = new EmployeeInfo(EmployeeInfo.SortMethod.BYNAME);
+		ei.sort(emps);
+		System.out.println(emps);
+		ei = new EmployeeInfo(EmployeeInfo.SortMethod.BYSALARY);
+		ei.sort(emps);
+		System.out.println(emps);*/
 	}
 }
 
--- MPP/src/lesson8/labs/prob2/A/EmployeeNameComparator.java
+++ MPP/src/lesson8/labs/prob2/A/EmployeeNameComparator.java
@@ -3,7 +3,7 @@ package lesson8.labs.prob2.A;
 import java.util.Comparator;
 
 /* A functor, but not a closure */
-public class EmployeeComparator implements Comparator<Employee> {
+public class EmployeeNameComparator implements Comparator<Employee> {
 	@Override
 	public int compare(Employee e1, Employee e2) {
 		if(e1.name.compareTo(e2.name) != 0)
