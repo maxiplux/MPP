67a3f8426f8bef68b51cc8afc53cd34f1d1eb89e
Daniel Trinh <kyle.sontrinh@gmail.com>
2018-07-11 13:44:03 +0700

    Personal

---
+++ MPP_Son/src/lesson8/labs/prob6/Apple.java
@@ -0,0 +1,13 @@
+package lesson8.labs.prob6;
+
+public class Apple {
+	Apple(double weight) {
+		this.weight = weight;
+	}
+
+	private double weight;
+
+	public double getWeight() {
+		return weight;
+	}
+}
