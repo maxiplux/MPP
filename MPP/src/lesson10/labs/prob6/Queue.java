6ca598bf038d8bc89e7e05eb392dca335df89928
Daniel Trinh <kyle.sontrinh@gmail.com>
2018-07-13 17:32:46 +0700

    Final solution lab6

--- MPP/src/lesson10/labs/prob6/Queue.java
+++ MPP/src/lesson10/labs/prob6/Queue.java
@@ -1,5 +1,8 @@
 package lesson10.labs.prob6;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 //Show the methods are not threadsafe.
 //Then modify so that they are threadsafe.
 public class Queue {
@@ -8,7 +11,6 @@ public class Queue {
 		Node next;
 
 	}
-
 	private Node head;
 	private Node tail;
 
@@ -67,32 +69,38 @@ public class Queue {
 
 		for (int i = 0; i <= 100; i++)
 			new Thread(r).start();
-
+		try {
+			Thread.sleep(10);
+		} catch(InterruptedException e) {}
 	}
 	
 	public static void multipleThreadSafe(Queue q) {
 
 		Runnable r = () -> {
-			for (int i = 0; i <= 100; i++)
+			for (int i = 0; i < 100; i++)
 				q.addThreadSafe(i);
 		};
 
-		for (int i = 0; i <= 100; i++)
+		for (int i = 0; i < 100; i++)
 			new Thread(r).start();
-
+		try {
+			Thread.sleep(10);
+		} catch(InterruptedException e) {}
 	}
 
 	public static void main(String[] args) {
+		System.out.println("Not Thread Safe");
 		Queue q1 = new Queue();
 		multipleThreadNotSafe(q1);
 		System.out.println(q1.getSize());
 
 		// -> the number of element is different every time we run multithread add
-		// -> the number of adds may be fewer or more than we expected when running
-		// multithread
+		// -> the number of adds may be fewer or more than 
+		// we expected when using multithread
 		// to add new nodes to queue
 		
 		//THREAD SAFE
+		System.out.println("Thread Safe");
 		Queue q2 = new Queue();
 		multipleThreadSafe(q2);
 		System.out.println(q2.getSize());
