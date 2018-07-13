60c3a141893e379f6e89a563bc96cc777d6f7440
Juan Francisco Mosquera <maxiplux@gmail.com>
2018-07-13 16:44:45 -0500

    fix

--- MPP/src/lesson10/labs/prob1/javapackage/ClosedCurve.java
+++ MPP/src/lesson10/labs/prob1/javapackage/ClosedCurve.java
@@ -2,7 +2,10 @@ package lesson10.labs.prob1.javapackage;
 
 import lesson10.labs.prob1.bugreporter.BugReport;
 
-@BugReport(assignedTo="Tom Jones", severity=1, reportedBy="Corazza")
+@BugReport(assignedTo="Tom Jones", 
+severity=1, 
+reportedBy="Corazza", 
+description = "The Complexity is N and the original requeriments said Nlogn")
 public interface ClosedCurve {
 	public double computePerimeter();
 }
