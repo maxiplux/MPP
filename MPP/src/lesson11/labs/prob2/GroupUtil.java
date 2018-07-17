fd473984bfcf5515f06bb07a76a641baa1b5affd
masterde2017 <masterde2017@gmail.com>
2018-07-16 22:58:13 -0500

    wh

--- MPP/src/lesson11/labs/prob2/GroupUtil.java
+++ MPP/src/lesson11/labs/prob2/GroupUtil.java
@@ -9,11 +9,22 @@ public class GroupUtil {
 //		return grp;
 //	}
 	
+	//Fix this code
+	public static Group<?> copy(Group<?> group) {
+		return auxilary(group);
+	}
+	
+	public static <T> Group<T>  auxilary(Group<T> group){
+		List<T> elements = group.getElements();
+		Group<T> grp = new Group<T>(group.getSpecialElement(), elements);
+		return grp;
+	}
+	
 	//Test using this main method
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(0,1,2,3,4);
 		Group<Integer> group = new Group<>(0, list);
 		System.out.println(group);
-		//System.out.println(GroupUtil.copy(group));
+		System.out.println(GroupUtil.copy(group));
 	}
 }
